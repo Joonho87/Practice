96c498f3e3068059b7e4400b1a586a68fbb934e9
Joonho Choi <jc379742@gmail.com>
2019-02-12 19:41:44 -0800

    Add files via upload
    uploaded java class

---
+++ StopSign.java
@@ -0,0 +1,46 @@
+public class StopSign {
+   public static void main(String[] args) {
+      egg();
+      teacup();
+      System.out.println();
+      stopSign();
+      System.out.println();
+      hat();
+   }
+   
+   public static void egg() {
+      top();
+      bottom();
+   }
+   
+   public static void teacup() {
+      bottom();
+      line();
+   }
+   
+   public static void stopSign() {
+      top();
+      System.out.println("| STOP |");
+      bottom();
+   }
+   
+   public static void hat() {
+      top();
+      line();
+   }
+   
+   public static void top() {
+      System.out.println("  ___");
+      System.out.println(" /   \\");
+      System.out.println("/     \\");
+   }
+   
+   public static void bottom() {
+      System.out.println("\\     /");
+      System.out.println(" \\___/");
+   }
+   
+   public static void line() {
+      System.out.println("+--------+");
+   }
+}
